36d66f3efb3d2445a8d045afda5b765bb45db805
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-08-15 06:29:12 +0530

    update win key function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
-const main = document.getElementsByClassName('menu_main');
-menu = document.getElementsByClassName('task_icon');
+'use strict';
+
+const main = document.querySelector('.menu_main');
+let menu = document.querySelector('.main_bar');
+
+let tops= getComputedStyle(main);
+
+menu.addEventListener('click',function () {
+        
+        if(tops.top == "-549px")
+        {
+            console.log("done");
+            main.style.top = "70px";
+        }
+        else if (tops.top == "70px") {
+            main.style.top = "-549px";
+        }
+    })
 
 
-    menu.addEventListener('click',()=>{
-        console.log("hi")
-        if(main.style.top == "-549px")
+
+
+
+document.addEventListener('keydown' , function(k) {
+   
+    if(k.key == 'W' || k.key == 'w')
+    {
+        if(tops.top == "-549px")
         {
             main.style.top = "70px";
         }
-    })
+        else if (tops.top == "70px") {
+            main.style.top = "-549px";
+        }
+    }
+
+})
+
